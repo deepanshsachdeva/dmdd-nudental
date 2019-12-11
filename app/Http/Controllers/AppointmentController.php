8ba8bb14ce004fd7ef821c36c32c4b1376dfe4c8
Deepansh Sachdeva <deepansh.99530@gmail.com>
2019-12-11 16:57:01 -0500

    appointment listing

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,13 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
 
 
 class AppointmentController extends Controller
 {
     public function index() {
-        return response()->view('appointments.index');
+        $locations = DB::select("select office_id, name from office");
+        $providers = DB::select("select provider_id, fname, lname from provider");
+        $patients = DB::select("select patient_id, fname, lname from patient");
+
+        $q_location = request()->input('location');
+        $q_provider = request()->input('provider');
+        $q_patient = request()->input('patient');
+        $q_status = request()->input('status');
+        $q_date = request()->input('date');
+
+        $query = "
+            select
+                appointment.appointment_id,
+                LEFT((string_agg(concat(provider.fname, ','), ' ')), LEN((string_agg(concat(provider.fname, ','), ' '))) - 1) as providers,
+                concat(patient.fname, ' ', patient.lname) as patient,
+                concat(office.name, ' - ', room.room_code) as location,
+                appointment.start_date,
+                appointment.end_date,
+                appointment.status,
+                appointment.next_appointment_id, 
+                appointment.created_at
+            from appointment 
+            inner join appointment_provider on appointment_provider.appointment_id = appointment.appointment_id
+            inner join provider on provider.provider_id = appointment_provider.provider_id
+            inner join patient on appointment.patient_id = patient.patient_id
+            inner join room on room.room_id = appointment.room_id
+            inner join office on office.office_id = room.office_id";
+
+        if ($q_location || $q_provider || $q_patient || $q_status || $q_status || $q_date) {
+            $query .= " where ";
+             
+            if ($q_location) {
+                $query .= (" office.office_id = ".$q_location);
+            }
+
+            if ($q_provider) {
+                $query .= (" provider.provider_id = ".$q_provider);
+            }
+            
+            if ($q_patient) {
+                $query .= (" patient.patient_id = ".$q_patient);
+            }
+
+            if ($q_status) {
+                $query .= (" appointment.status = ".$q_status);
+            }
+
+            if ($q_date) {
+                $query .= (" cast(appointment.start_date as date) = '".$q_date."'");
+            }
+        }
+        
+        $group = " group by 
+            appointment.appointment_id, 
+            appointment.start_date, 
+            appointment.end_date, 
+            appointment.status, 
+            appointment.next_appointment_id, 
+            appointment.created_at, 
+            patient.lname, 
+            patient.fname, 
+            office.name, 
+            room.room_code
+        ";
+
+        $query = $query.$group;
+
+        // dd($query);
+
+        $appointments = DB::select($query);
+
+        return response()->view('appointments.index', compact('appointments', 'locations', 'providers', 'patients'));
     }
 
     public function createForm() {
--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -11,88 +11,87 @@
             <a href="{{ route('appointments.new') }}" class="btn btn-primary" role="button">+ New</a>
         </div>
     </div>
-    <form action="">
+    <form action="{{ route('appointments.index') }}" method="GET">
         <div class="form-row">
             <div class="form-group col">
                 <label>Location</label>
-                <select class="form-control">
-
-                    <option>Default select </option>
+                <select class="form-control" name="location">
+                    <option value="">--- Select Location ---</option>
+                    @foreach($locations as $location)
+                    <option value="{{ $location->office_id }}" @if(request()->input('location') == $location->office_id)) selected @endif>{{ $location->name }}</option>
+                    @endforeach
                 </select>
             </div>
-
             <div class="form-group col">
                 <label>Provider</label>
-                <select class="form-control">
-                    <option>Default select </option>
+                <select class="form-control" name="provider">
+                    <option value="" selected>--- Select Provider ---</option>
+                    @foreach($providers as $provider)
+                    <option value="{{ $provider->provider_id }}" @if(request()->input('provider') == $provider->provider_id)) selected @endif>{{ $provider->fname }} {{ $provider->lname }}</option>
+                    @endforeach
                 </select>
             </div>
-
             <div class="form-group col">
                 <label>Patient</label>
-                <select class="form-control">
-                    <option>Default select </option>
+                <select class="form-control" name="patient">
+                    <option value="">--- Select Patient ---</option>
+                    @foreach($patients as $patient)
+                    <option value="{{ $patient->patient_id }}" @if(request()->input('patient') == $patient->patient_id)) selected @endif>{{ $patient->fname }} {{ $patient->lname }}</option>
+                    @endforeach
                 </select>
             </div>
-
             <div class="form-group col">
-                <label>status</label>
-                <select class="form-control">
-                    <option>Default select </option>
+                <label>Status</label>
+                <select class="form-control" name="status">
+                    <option value="">--- Select Status ---</option>
+                    <option value="B" @if(request()->input('status') == "B")) selected @endif>Booked</option>
+                    <option value="O" @if(request()->input('status') == "O")) selected @endif>Open</option>
+                    <option value="C" @if(request()->input('status') == "C")) selected @endif>Cancelled</option>  
                 </select>
             </div>
-
             <div class="form-group col">
                 <label>Date</label>
-                <select class="form-control">
-                    <option>Default select </option>
-                </select>
+                <input class="form-control" type="text" name="date" value="{{ request()->input('date') }}">
             </div>
-
-
         </div>
         <div class="form-group text-right ">
-            <a href="#" class="btn btn-primary" role="button">Filter</a>
-            <a href="#" class="btn btn-secondary" role="button">Reset</a>
+            <button type="submit" class="btn btn-primary">Filter</button>
+            <a href="{{ route('appointments.index') }}" class="btn btn-secondary" role="button">Reset</a>
         </div>
     </form>
-
-
     <div class="row">
-
         <div class="col">
-
             <table class="table table-striped">
                 <thead class="thead-dark">
                     <tr>
-                        <th scope="col">Appointment Id</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Providers</th>
                         <th scope="col">Patient</th>
-                        <th scope="col">Provider</th>
                         <th scope="col">Location</th>
                         <th scope="col">Start</th>
                         <th scope="col">End</th>
                         <th scope="col">Status</th>
+                        <th scope="col">Next Appointment</th>
                         <th scope="col">Created On</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                    @foreach($appointments as $appointment)
                     <tr>
-                        <td scope="row"></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td scope="row"><a href="#">NU{{ $appointment->appointment_id }}</a></td>
+                        <td>{{ $appointment->providers}}</td>
+                        <td>{{ $appointment->patient}}</td>
+                        <td>{{ $appointment->location }}</td>
+                        <td>{{ $appointment->start_date }}</td>
+                        <td>{{ $appointment->end_date }}</td>
+                        <td>{{ $appointment->status }}</td>
+                        <td>{{ ($appointment->next_appointment_id) ? 'Yes' : 'No' }}</td>
+                        <td>{{ $appointment->created_at }}</td>
                     </tr>
-
+                    @endforeach
                 </tbody>
             </table>
-
         </div>
-
     </div>
 </div>
 
